815b5a9ae2dc855df343bf8d5057a5398b8d34d2
mini-360 <rajabhay56999@gmail.com>
2024-08-07 16:34:12 +0530

    chore: Add auth middleware for user authentication

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import { getUser } from "../service/auth.service.js";
+
+async function restrictToLoggedinUserOnly(req, res, next) {
+  const userUid = req.cookies?.uid;
+
+  if (!userUid) return res.redirect("/login");
+  const user = getUser(userUid);
+
+  if (!user) return res.redirect("/login");
+
+  req.user = user;
+  next();
+}
+
+async function checkAuth(req, res, next) {
+  const userUid = req.cookies?.uid;
+
+  const user = getUser(userUid);
+
+  req.user = user;
+  next();
+}
+
+export { restrictToLoggedinUserOnly, checkAuth };
